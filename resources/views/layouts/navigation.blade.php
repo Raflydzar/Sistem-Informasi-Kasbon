2756e8319b646936c74939b163534bed983181eb
raflydzar <raflydzar13@gmail.com>
2026-08-20 14:30:19 +0800

    update hamburger button

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -11,14 +11,33 @@
                 </div>
 
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
-                    <x-nav-link :href="route('unit.index')" :active="request()->routeIs('unit.*')">Unit</x-nav-link>
-                    <x-nav-link :href="route('transaksi.index')" :active="request()->routeIs('transaksi.*')">Transaksi</x-nav-link>
-                    <x-nav-link :href="route('laporan.index')" :active="request()->routeIs('laporan.*')">Laporan</x-nav-link>
-                </div>
+                <!-- Responsive Navigation Menu (Tampilan Mobile) -->
+<div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
+    <div class="pt-2 pb-3 space-y-1">
+        <!-- Dashboard -->
+        <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+            {{ __('Dashboard') }}
+        </x-responsive-nav-link>
+
+        <!-- Unit -->
+        <x-responsive-nav-link :href="route('unit.index')" :active="request()->routeIs('unit.*')">
+            {{ __('Unit') }}
+        </x-responsive-nav-link>
+
+        <!-- Transaksi -->
+        <x-responsive-nav-link :href="route('transaksi.index')" :active="request()->routeIs('transaksi.*')">
+            {{ __('Transaksi') }}
+        </x-responsive-nav-link>
+
+        <!-- Laporan -->
+        <x-responsive-nav-link :href="route('laporan.index')" :active="request()->routeIs('laporan.*')">
+            {{ __('Laporan') }}
+        </x-responsive-nav-link>
+    </div>
+
+    <!-- Responsive Settings Options (Profil & Logout) -->
+    ...
+</div>
             </div>
 
             <!-- Settings Dropdown -->
