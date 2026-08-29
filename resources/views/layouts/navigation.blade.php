e28a4192c3a0d2b52a8230e99c40cb782982d542
raflydzar <raflydzar13@gmail.com>
2026-08-29 21:21:26 +0800

    update logo navbar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,13 +12,6 @@
                     </button>
                 </div>
 
-                <!-- Logo -->
-                <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
-                    </a>
-                </div>
-
                 <!-- Navigation Links (Desktop Atas) -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-6 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
