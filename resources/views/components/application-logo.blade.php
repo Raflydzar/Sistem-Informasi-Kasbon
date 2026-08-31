d406df61f4b53eae74ed960dab6c6cc73548f099
raflydzar <raflydzar13@gmail.com>
2026-08-31 14:42:46 +0800

    update size logo login/signup

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,1 +1,1 @@
-<img src="{{ asset('mci.png') }}" {{ $attributes->merge(['class' => 'w-20 h-20 object-contain']) }} alt="Logo">
+<img src="{{ asset('mci.png') }}" {{ $attributes->merge(['class' => 'h-28 w-auto object-contain']) }} alt="Logo">
