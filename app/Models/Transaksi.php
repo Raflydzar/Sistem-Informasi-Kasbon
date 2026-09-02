c884f4458bafbfef3f5fc00aeaef9ddbcd2993fc
raflydzar <raflydzar13@gmail.com>
2026-09-02 10:36:20 +0800

    debug riwayat transaksi

--- app/Models/Transaksi.php
+++ app/Models/Transaksi.php
@@ -2,13 +2,26 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Transaksi extends Model
 {
+    use HasFactory;
+    
     protected $fillable = [
-        'kas_id', 'unit_id','tanggal',
-        'deskripsi','no_nota','volume','kode_unit','jenis','nominal', 'saldo','kategori_utama','sub_kategori'
+        'kas_id',
+        'unit_id',
+        'tanggal',
+        'deskripsi',
+        'no_nota',
+        'volume',
+        'kode_unit',
+        'jenis',
+        'nominal',
+        'saldo',
+        'kategori_utama',
+        'sub_kategori'
     ];
     public function kas()
     {
